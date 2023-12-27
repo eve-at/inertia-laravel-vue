ca3f4191a49a9526d19c588f7f5a14de65f7a0ce
Alexandre Muraviov <amuraviov@globalia.ca>
2023-12-27 08:27:23 -0500

    move nav link stuff for a dedicated component

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,19 +1,38 @@
 <script>
 import { Link } from '@inertiajs/vue3';
+import NavLink from './NavLink.vue';
 
 export default {
     props: {},
-    components: { Link }
+    components: { Link, NavLink }
 };
 </script>
 
 <template>
     <nav class="mt-6">
         <ul class="list-disc">
-            <li><Link class="text-blue-500 hover:underline" href="/">Home</Link></li>
-            <li><Link class="text-blue-500 hover:underline" href="/users">Users</Link></li>
-            <li><Link class="text-blue-500 hover:underline" href="/settings">Settings</Link></li>
-            <li><Link class="text-blue-500 hover:underline" href="/logout" method="post" as="button">Log Out</Link></li>
+            <li>
+                <NavLink href="/" :active="$page.component === 'Home'">
+                    Home
+                </NavLink>            
+            </li>
+            <li>
+                <NavLink href="/users" :active="$page.component === 'Users'">
+                    Users
+                </NavLink>
+            </li>
+            <li>
+                <NavLink href="/settings" :active="$page.component === 'Settings'">
+                    Settings
+                </NavLink>
+            </li>
+            <li>
+                <Link 
+                    class="text-blue-500 hover:underline" href="/logout" method="post" as="button"
+                >
+                    Log Out
+                </Link>
+            </li>
         </ul>
     </nav>
 </template>
